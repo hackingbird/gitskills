a1eef125ae40dd5fda3c4546c444f81d2914f730
hackingbird <903867028@qq.com>
2019-09-27 16:21:13 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,24 +26,21 @@ class SessionsController extends Controller
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
-//validate 方法接收两个参数，第一个参数为用户的输入数据，第二个参数为该输入数据的验证规则
 
-//attempt 方法会接收一个数组来作为第一个参数，该参数提供的值将用于寻找数据库中的用户数据
-if (Auth::attempt($credentials, $request->has('remember'))) {
-    
-    session()->flash('success', '欢迎回来！');
-    $fallback = route('users.show', Auth::user());
-    return redirect()->intended($fallback);
-    
-    
-} else {
-    // 登录失败后的相关操作
-    session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-    return redirect()->back()->withInput();
-    //跳转到上个页面，并携带错误信息
-}
-
-       return;
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+            if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               $fallback = route('users.show', Auth::user());
+               return redirect()->intended($fallback);
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back()->withInput();
+       }
     }
 
     public function destroy()
